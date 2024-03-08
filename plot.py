500026bbe2a0aea613f65e764de5e1ea84b1bc26
PJalv <pjalvbusiness@gmail.com>
2024-03-07 19:06:28 -0800

    Update Plot script

--- plot.py
+++ plot.py
@@ -1,21 +1,17 @@
+import sys
 import json
 import matplotlib.pyplot as plt
-from matplotlib.patches import ConnectionPatch
 
-from matplotlib.patches import Rectangle
 
 def plot_tree(node, x, y, level=0, parent_x=None):
     if not node:
         return
 
-    # Plot current node
     plt.text(x, y, '\n'.join([' '.join(row) for row in node['data']]), ha='center', va='center')
-    # plt.gca().add_patch(Rectangle((x - 2, y - 0.3), 4, 0.75, edgecolor='black', facecolor='none'))
 
-    # Plot children
     if node['children']:
         num_children = len(node['children'])
-        child_spacing = 2.0 ** (4 - level) * 2  # Adjust spacing based on level and multiply by 2 to create space between boxes
+        child_spacing = 2.0 ** (4 - level) * 2 
         total_width = num_children * child_spacing
         start_x = x - total_width / 2
         for child in node['children']:
@@ -24,12 +20,16 @@ def plot_tree(node, x, y, level=0, parent_x=None):
             plt.plot([x, child_x], [y - 0.5, y - 1], 'k-', lw=1)
             start_x += child_spacing
 
-# Load tree data from JSON file
-with open('tree.json') as f:
+if len(sys.argv) != 2:
+    print("Usage: python script.py <filename>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+with open(filename) as f:
     tree_data = json.load(f)
 
-# Create figure and plot tree
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(15, 8))
 plt.axis('off')
 plot_tree(tree_data, 0, 0)
 plt.show()
